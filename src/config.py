27628331f46bb536c7abb7fb043a93f65d24534d
Aviya Keisar <aviyak43@gmail.com>
2025-02-07 11:16:00 +0200

    added config file

---
+++ src/config.py
@@ -0,0 +1,4 @@
+# CONSTANTS
+ITERATIONS = 10000  # according to Yaara - this is enough to replicate the results
+RES_DIR = '/home/eng/keisara/Python-Workshop/results'
+SAVE_DIR = '/home/eng/keisara/Python-Workshop/bands_results_df'
--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 from analysis.run_for_all_patients import run_analysis_for_all_patients
 from df_for_visualization import convert_mat_results_to_df
+from config import ITERATIONS, RES_DIR, SAVE_DIR
 
-
-# CONSTANTS
-ITERATIONS = 10000  # according to Yaara - this is enough to replicate the results
-RES_DIR = '/home/eng/keisara/Python-Workshop/results'
-SAVE_DIR = '/home/eng/keisara/Python-Workshop/bands_results_df'
 
 def main(res_dir=RES_DIR, save_dir=SAVE_DIR, iterations=ITERATIONS):
     run_analysis_for_all_patients(res_dir, iterations)
